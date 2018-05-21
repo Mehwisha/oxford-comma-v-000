2f6f1368ea814483b805190cb1380fbe57d3b12e
Mehwish Aziz <mehwisha@msn.com>
2018-05-21 04:10:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1
       return array[0]
     elsif array.length > 3
-    return array[0..-3].join(', ') + " and " + array[-2]
+    return array[0..-2].join(', ') + " and " + array[-1]
 
   end
 end
