bb4052e6e1235fdcc147122107e135fc48620a1c
Mehwish Aziz <mehwisha@msn.com>
2018-05-21 04:10:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
 
   if array.length == 1
       return array[0]
-    elsif array.length > 1
-    return array[0..-2].join(', ') + " and " + array[-1]
+    elsif array.length > 3
+    return array[0..-3].join(', ') + " and " + array[-2]
 
   end
 end
